7559ff8169985044280c0660a78af87efa9c16ab
onuyonnu <sushioman.OD@gmail.com>
2022-03-02 12:57:46 -0500

    Update fun_bits_all_colors.py

--- Art_git/fun_bits_all_colors.py
+++ Art_git/fun_bits_all_colors.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 
 response = requests.get('https://htmlcolorcodes.com/colors/').text
 soup = BeautifulSoup(response, "lxml")
-
+#Pulls a master list of colors from htmlcolorcodes.com
 colors = []
 for color in soup.find_all("td"):
     try:
@@ -14,20 +14,20 @@ for color in soup.find_all("td"):
     except Exception as e:
         pass
     colors.append(Hex)
+#turtle import setup the gui
 wn = turtle.Screen()
 wn.bgcolor("black")
 ollie = turtle.Turtle()
 ollie.speed(0)
 ollie.pensize(1)
 rtimes = random.randint(6, 10)
+#randomizes the size shape and frequency of the patern
 for i in range(rtimes):
     random.shuffle(colors)
     rsides = random.randint(3, 10)
     rangles = random.randint(15, 30)
     rtimes2 = random.randint(5, 30)
     rlenght = random.randint(50, 250)
-    # ollie.forward(rsides)
-    # ollie.left(rtimes2)
     for i in colors[:2]:
         print(i)
         ollie.pencolor(i)
